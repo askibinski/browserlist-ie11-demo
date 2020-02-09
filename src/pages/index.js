5fdae94f8cd2b4bda7591d23de4d41b1efdddb4f
askibinski <albert.skibinski@intracto.com>
2020-02-09 10:52:38 +0100

    getFood

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,27 +3,21 @@ import React from "react"
 import Layout from "../components/layout"
 
 
-const foodItems = [
-  "pizza",
-  "hamburger",
-  "pickles",
-  "gluten-free-waffles"
-];
-
-const food = foodItems.find(food => food.startsWith("p"));
-
-const getColor = function() {
-  alert(['red', 'orange', 'green'].find(color => color.startsWith('o')));
+const getFood = function() {
+  alert([
+    "pizza",
+    "hamburger",
+    "pickles",
+    "gluten-free-waffles"
+  ].find(food => food.startsWith('p')));
 };
 
 const IndexPage = () => (
 
   <Layout>
-
-    <h1>{food}</h1>
-
-    <button onClick={getColor}>What color?</button>
-
+    <p>
+      <button onClick={getFood}>What food?</button>
+    </p>
   </Layout>
 
 );
