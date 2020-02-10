9f001e2d035076e4e4817f690c433a6cec621550
askibinski <albert.skibinski@intracto.com>
2020-02-10 19:15:21 +0100

    added object.values call

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import Layout from "../components/layout"
 
-
 const getFood = function() {
   alert([
     "pizza",
@@ -12,11 +11,27 @@ const getFood = function() {
   ].find(food => food.startsWith('p')));
 };
 
+const refrigerator = {
+  pizza: 2,
+  carrot: 1,
+  cheese: 3,
+  wine: 4,
+  beer: 6,
+};
+
+const getRefrigerator = function() {
+  alert(Object.values(refrigerator));
+};
+
+
 const IndexPage = () => (
 
   <Layout>
     <p>
       <button onClick={getFood}>What food?</button>
+    </p>
+    <p>
+      <button onClick={getRefrigerator}>Get refrigerator</button>
     </p>
   </Layout>
 
